9e30c364221b74cf2a2e3d540a9d5f0763829537
Riyad Abdullayev <riyadabdullayev57@gmail.com>
2025-02-12 16:11:17 +0400

    task 1.2 bitdi

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# Tipleri göstər
 number=12 #integer type
 float_number=34.66 #float type
 my_list=["kitab","qelem"] #list type
@@ -6,3 +7,9 @@ print(f"""
     {float_number}`in tipi: {type(float_number)},
     {my_list}`in tipi: {type(my_list)}
     """)
+
+# 3 dəyərdən ibarət list yarat daha sonra bir dəyəri sil, sonra sildiyin dəyəri əlavə et yenidən
+ilist = ["men", "sen", "biz"]  # List yarat
+ilist.remove("men")  # "men" elementini sil
+ilist.append("size")  # Siyahıya "size" əlavə et
+print(ilist)
