f8c03e98f297ed1349ac0ea77db7fd194097965e
Riyad Abdullayev <riyadabdullayev57@gmail.com>
2025-02-12 16:00:25 +0400

    Tipleri goster, taski bitdi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ number=12 #integer type
 float_number=34.66 #float type
 my_list=["kitab","qelem"] #list type
 print(f"""
-    {type(number)},
-    {type(float_number)},
-    {type(my_list)}
+    {number}`in tipi: {type(number)},
+    {float_number}`in tipi: {type(float_number)},
+    {my_list}`in tipi: {type(my_list)}
     """)
